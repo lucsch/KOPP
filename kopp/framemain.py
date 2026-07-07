989a21fbe1280bcea3a545f089996cbc86e45889
Lucien <lucien.schreiber@terranum.ch>
2026-07-07 12:44:42 +0200

    Adding UI skeleton

--- kopp/framemain.py
+++ kopp/framemain.py
@@ -6,6 +6,8 @@ import wx
 import wx.dataview
 import wx.svg
 
+from kopp.framemainlist import FrameMainListView
+
 from kopp.version import COMMIT_NUMBER
 from kopp.version import VERSION_MAJOR_MINOR
 
@@ -22,4 +24,80 @@ class FrameMain(wx.Frame):
         self.m_status_bar.SetStatusWidths([200, -1, 150])
         self.SetStatusBarPane(-1)
         self.m_status_bar.SetStatusText("version {}.{}".format(VERSION_MAJOR_MINOR, COMMIT_NUMBER), 2)
+
+        self._create_menubar()
+        self._create_toolbar()
+        self._create_controls()
+
+
+    def _create_menubar(self):
+        self.m_menubar = wx.MenuBar(0)
+        self.m_menu_file = wx.Menu()
+        self.m_menui_file_new = wx.MenuItem(self.m_menu_file, wx.ID_NEW, _(u"New"), wx.EmptyString, wx.ITEM_NORMAL)
+        self.m_menu_file.Append(self.m_menui_file_new)
+
+        self.m_menui_file_open = wx.MenuItem(self.m_menu_file, wx.ID_OPEN, _(u"Open...") + u"\t" + u"Ctrl+O",
+                                             wx.EmptyString, wx.ITEM_NORMAL)
+        self.m_menu_file.Append(self.m_menui_file_open)
+
+        self.m_menu_file_recent = wx.Menu()
+        self.m_menu_file.AppendSubMenu(self.m_menu_file_recent, _(u"Recent"))
+
+        self.m_menu_file.AppendSeparator()
+
+        self.m_menui_file_exit = wx.MenuItem(self.m_menu_file, wx.ID_EXIT, _(u"Quit"), wx.EmptyString, wx.ITEM_NORMAL)
+        self.m_menu_file.Append(self.m_menui_file_exit)
+
+        self.m_menubar.Append(self.m_menu_file, _(u"&File"))
+
+        self.m_menu_rec = wx.Menu()
+        self.m_menui_rec_add = wx.MenuItem(self.m_menu_rec, wx.ID_ANY, _(u"Add...") + u"\t" + u"Ctrl+N", wx.EmptyString,
+                                           wx.ITEM_NORMAL)
+        self.m_menu_rec.Append(self.m_menui_rec_add)
+
+        self.m_menui_rec_delete = wx.MenuItem(self.m_menu_rec, wx.ID_ANY, _(u"Delete") + u"\t" + u"DEL", wx.EmptyString,
+                                              wx.ITEM_NORMAL)
+        self.m_menu_rec.Append(self.m_menui_rec_delete)
+
+        self.m_menui_rec_edit = wx.MenuItem(self.m_menu_rec, wx.ID_ANY, _(u"Edit...") + u"\t" + u"ENTER",
+                                            wx.EmptyString, wx.ITEM_NORMAL)
+        self.m_menu_rec.Append(self.m_menui_rec_edit)
+
+        self.m_menubar.Append(self.m_menu_rec, _(u"Records"))
+
+        self.m_menu_view = wx.Menu()
+        self.m_menu_view_info = wx.MenuItem(self.m_menu_view, wx.ID_ANY, _(u"Info window") + u"\t" + u"Ctrl+I",
+                                            wx.EmptyString, wx.ITEM_CHECK)
+        self.m_menu_view.Append(self.m_menu_view_info)
+        self.m_menu_view_info.Check(True)
+
+        self.m_menubar.Append(self.m_menu_view, _(u"View"))
+
+        self.m_menui_help = wx.Menu()
+        self.m_menui_help_about = wx.MenuItem(self.m_menui_help, wx.ID_ABOUT, _(u"About"), wx.EmptyString,
+                                              wx.ITEM_NORMAL)
+        self.m_menui_help.Append(self.m_menui_help_about)
+
+        self.m_menui_help_web = wx.MenuItem(self.m_menui_help, wx.ID_ANY, _(u"Website..."), wx.EmptyString,
+                                            wx.ITEM_NORMAL)
+        self.m_menui_help.Append(self.m_menui_help_web)
+
+        self.m_menubar.Append(self.m_menui_help, _(u"Help"))
+
+        self.SetMenuBar(self.m_menubar)
+
+    def _create_toolbar(self):
+        pass
+
+    def _create_controls(self):
+        self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
+
+        bSizer1 = wx.BoxSizer(wx.VERTICAL)
+
+        self.m_list = FrameMainListView(self, wx.ID_ANY)
+        bSizer1.Add(self.m_list, 1, wx.ALL | wx.EXPAND, 5)
+
+        self.SetSizer(bSizer1)
+        self.Layout()
+        self.Centre(wx.BOTH)
 
---
+++ kopp/framemainlist.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+
+from collections import OrderedDict
+
+import wx
+import wx.dataview
+
+
+
+class MainListViewData:
+    """store data for the main list (columns name and width"""
+
+    column_info = OrderedDict([
+        ("Date", 150),
+        ("HR", 80),
+        ("A", 80),
+        ("VAC", 80),
+        ("Error", 150),
+        ("Comment", 200),
+    ])
+
+
+class FrameMainListView(wx.dataview.DataViewListCtrl):
+    """
+    Class for the data view
+    """
+
+    def __init__(self, parent, identifier):
+        wx.dataview.DataViewListCtrl.__init__(self, parent, identifier, wx.DefaultPosition, wx.DefaultSize,
+                                              wx.dataview.DV_MULTIPLE | wx.dataview.DV_ROW_LINES)
+        """Constructor for DataView"""
+        self.parent = parent
+        self.m_frame_view = None
+        self.m_img_path = None
+        #self.m_imgdata: list[ImageData] = []
+
+        # create the columns
+        for my_category, my_width in MainListViewData.column_info.items():
+            self.AppendTextColumn(my_category, width=my_width)
+
+        # self.Bind(wx.dataview.EVT_DATAVIEW_ITEM_ACTIVATED, self.on_comments)
+        # self.Bind(wx.dataview.EVT_DATAVIEW_SELECTION_CHANGED, self.on_selection_change)
+        #
+        # # Bind Ctrl+Up/Down events
+        # accel_entries = [
+        #     wx.AcceleratorEntry(wx.ACCEL_CTRL, wx.WXK_UP, wx.NewIdRef()),
+        #     wx.AcceleratorEntry(wx.ACCEL_CTRL, wx.WXK_DOWN, wx.NewIdRef())
+        # ]
+        # accel_table = wx.AcceleratorTable(accel_entries)
+        # self.SetAcceleratorTable(accel_table)
+        # self.Bind(wx.EVT_MENU, self.on_select_previous_valid, id=accel_entries[0].GetCommand())
+        # self.Bind(wx.EVT_MENU, self.on_select_next_valid, id=accel_entries[1].GetCommand())
+
+    # def InvertSelection(self):
+    #     for index in range(self.GetItemCount()):
+    #         if self.IsRowSelected(index) is False:
+    #             self.SelectRow(index)
+    #         else:
+    #             self.UnselectRow(index)
+    #
+    # def add_item(self, data: ImageData):
+    #     if not data:
+    #         return
+    #     self.m_imgdata.append(data)
+    #     list_id = len(self.m_imgdata) - 1
+    #     self.AppendItem([data.get_date_str(), data.get_time_str(), data.get_short_filename(), "", "", ""], list_id)
+    #
+    # def on_selection_change(self, event):
+    #     if not self.m_frame_view or not self.m_img_path:
+    #         return
+    #
+    #     if self.GetSelectedItemsCount() != 1:
+    #         return
+    #
+    #     # get imagedata
+    #     row_id = self.GetSelectedRow()
+    #     list_id = self.GetItemData(self.RowToItem(row_id))
+    #     img_data = self.m_imgdata[list_id]
+    #     self.m_frame_view.set_image(img_data)
+    #
+    # def on_delete_items(self, event):
+    #     if self.GetSelectedItemsCount() == 0:
+    #         return
+    #     for selected_item in self.GetSelections():
+    #         # get row for item
+    #         row_id = self.ItemToRow(selected_item)
+    #         list_id = self.GetItemData(selected_item)
+    #         self.m_imgdata[list_id].m_delete = True
+    #         self.SetTextValue("X", row_id, 3)
+    #
+    # def on_restore_items(self, event):
+    #     if self.GetSelectedItemsCount() == 0:
+    #         return
+    #     for selected_item in self.GetSelections():
+    #         row_id = self.ItemToRow(selected_item)
+    #         list_id = self.GetItemData(selected_item)
+    #         self.m_imgdata[list_id].m_delete = False
+    #         self.SetTextValue("", row_id, 3)
+    #
+    # def on_comments(self, event):
+    #     if self.GetSelectedItemsCount() != 1:
+    #         return
+    #
+    #     selected_item = self.GetSelection()
+    #     row_id = self.ItemToRow(selected_item)
+    #     list_id = self.GetItemData(selected_item)
+    #
+    #     # display a TextEntryDialog for writing a comment
+    #     dlg = wx.TextEntryDialog(self, 'Enter a comment:', 'Comment',
+    #                              self.m_imgdata[list_id].m_comment)
+    #     if dlg.ShowModal() == wx.ID_OK:
+    #         self.m_imgdata[list_id].m_comment = dlg.GetValue()
+    #         self.SetTextValue(dlg.GetValue(), row_id, 4)
+    #     dlg.Destroy()
+    #
+    # def get_unique_selected_item(self):
+    #     if self.GetSelectedItemsCount() != 1:
+    #         return None
+    #     selected_item = self.GetSelection()
+    #     row_id = self.ItemToRow(selected_item)
+    #     list_id = self.GetItemData(selected_item)
+    #     return self.m_imgdata[list_id]
+    #
+    # def get_filenames(self, selected_only: bool = False):
+    #     filenames = []
+    #     for i in range(self.GetItemCount()):
+    #         if selected_only and not self.IsRowSelected(i):
+    #             continue
+    #         filenames.append(self.GetTextValue(i, 2))
+    #     return filenames
+    #
+    # def update_list(self):
+    #     self.DeleteAllItems()
+    #     for index, item in enumerate(self.m_imgdata):
+    #         delete_str = ""
+    #         if item.m_delete:
+    #             delete_str = "X"
+    #         self.AppendItem([item.get_date_str(), item.get_time_str(), item.get_short_filename(),
+    #                          delete_str, item.m_comment, item.m_info], index)
+    #
+    # def set_text_value(self, value, row_id, column_id):
+    #     self.SetValue(value, row_id, column_id)
+    #
+    # def get_not_deleted_indexes(self):
+    #     not_deleted_indexes = []
+    #     for index, item in enumerate(self.m_imgdata):
+    #         if not item.m_delete:
+    #             not_deleted_indexes.append(index)
+    #     return not_deleted_indexes
+    #
+    # def on_select_next_valid(self, event):
+    #     if self.GetSelectedItemsCount() != 1:
+    #         return
+    #     selected_item = self.GetSelection()
+    #     row_id = self.ItemToRow(selected_item)
+    #     list_id = self.GetItemData(selected_item)
+    #     for index, item in enumerate(self.m_imgdata[list_id+1:], list_id+1):
+    #         if not item.m_delete:
+    #             self.SelectRow(index)
+    #             self.UnselectRow(row_id)
+    #             self.on_selection_change(None)
+    #             return
+    #
+    # def on_select_previous_valid(self, event):
+    #     if self.GetSelectedItemsCount() != 1:
+    #         return
+    #     selected_item = self.GetSelection()
+    #     row_id = self.ItemToRow(selected_item)
+    #     list_id = self.GetItemData(selected_item)
+    #     for index in range(list_id - 1, -1, -1):
+    #         if not self.m_imgdata[index].m_delete:
+    #             self.SelectRow(index)
+    #             self.UnselectRow(row_id)
+    #             self.on_selection_change(None)
+    #             return
